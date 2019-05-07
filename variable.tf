fe2951087f7357878cf38f00023b6c0c4fc5b949
EC2 Default User <ec2-user@ip-172-31-33-252.ap-northeast-1.compute.internal>
2019-05-07 08:24:57 +0000

    first commit

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "amazon_linux" {
+  # Amazon Linux AMI 2017.03.1 (HVM), SSD Volume Type -ami-0f9ae750e8274075b
+  default = "ami-0f9ae750e8274075b"
+}
+
+variable "user08-Key" {
+  default = "user08-Key"
+}
